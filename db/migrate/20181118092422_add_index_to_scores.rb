855508e00d6ac60bf41c41275a8344d4d9fb9253
Ttoshinari <freeworker1105@gmail.com>
2018-11-21 13:07:31 +0900

    User.cul.1

--- db/migrate/20181118092422_add_index_to_scores.rb
+++ db/migrate/20181118092422_add_index_to_scores.rb
@@ -1,4 +1,10 @@
 class AddIndexToScores < ActiveRecord::Migration[5.0]
   def change
+    create_table :users do |t|
+    t.string :name,               null: false, unique: true
+      t.string :email,              null: false, default: ""
+      t.string :encrypted_password, null: false, default: ""
   end
+end
+
 end
