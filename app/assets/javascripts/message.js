66587b260a612e55acd02e87fb817d821cee6357
Ttoshinari <freeworker1105@gmail.com>
2018-11-27 10:07:44 +0900

    Edit.Ajax.line
    [What]
    メッセージ送信の非同期化
    自動スクロール
    [Why]
    非同期を理解するため

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,23 +1,35 @@
 $(function(){
-  $function buildHTML(comment){
-    var html = `.message-header-user-name
-    = message.user_name
-    .message-header-date
-      = message.created_at
-  .message-body
-    - if message.text.present?
-      %p.message-body-content
-        = message.text
-  = image_tag message.image.url, class: 'lower-message__image' if message.image.present?
+  function buildHTML(message){
+    var message_image = ''
+    if (message.image){
+    message_image = `<img src="${message.image}" class="lower-message__image" >`
+    }
+
+    var html =
+    `<div class="message">
+        <div class = "message-header-user-name">
+            ${message.user_name}
+        </div>
+          <div class = "message-header-date">
+            ${message.created_at}
+          </div>
+
+        <div class = "message-body">
+          <p class ="message-body-content">${message.text}</p>
+        </div>
+        ${message_image}
+      </div>
 `
     return html;
   }
 
-
   $('#new_message').on('submit', function(e){
-    // e.preventDefault();
+    e.preventDefault();
     var formData = new FormData(this);
     var url = $(this).attr("action")
+
+    $('form__submit').removeAttr('data-disable-with');
+
     $.ajax({
       type: "POST",
       url: url,
@@ -28,11 +40,21 @@ $(function(){
     })
 .done(function(data){
   var html = buildHTML(data);
-  $(".form__message").append(html)
-  $(".message").val("")
-    })
+  $(".content").append(html);
+  $(".form__message").val("")
+  $('.content').animate({scrollTop: $(".content")[0].scrollHeight}, 1500);
+   $('input').prop('disabled', false);
+  })
 .fail(function(){
       alert('error');
+ $('input').prop('disabled', false);
     })
+return false;
   })
 });
+
+
+
+// $(".form__submit").prop('disabled', true);
+
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
- root 'groups#index'
   devise_for :users
   root 'groups#index'
   resources :users, only: [:index,:edit, :update]
