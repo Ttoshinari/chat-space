93b488eec39e670793d04b893c805edc35b6aedd
Ttoshinari <freeworker1105@gmail.com>
2018-11-29 17:30:22 +0900

    Edit.incremental

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
 
 
 def index
-  @users = User.where('name LIKE(?)', "%#{params[:name]}%")
+  @users = User.where('name LIKE(?)', "%#{params[:name]}%", "id ≠ (#{current_user.id})")
     respond_to do |format|
       format.json
     end
